d77d8eba9bba93227314c287e7eb372079e8a2db
kpcastillo <krn_iveth09@hotmail.com>
2026-02-04 22:04:20 -0800

    Added picture for seller profile, worked on profile page

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -2,9 +2,20 @@
 export default async function ProfilePage() {
 
   return (
-      <div>
-        <h2>Curated collection</h2>
-          <p>No products yet.</p>
+    <main>
+      <div className="grid grid-cols-2 items-center gap-2 m-4">
+        <img className="rounded-full" src="/seller.png" alt="Profile Picture" width={150} height={150} />
+        <h1 className="text-2xl font-bold">Welcome Artisan</h1>
       </div>
+      <div>
+        <h2 className="text-xl font-semibold m-3">Story</h2>
+        <p>No story yet.</p>
+      </div>
+      <div>
+        <h2 className="text-xl font-semibold m-3">Products</h2>
+        <p>No products yet.</p>
+      </div>
+    </main>
+      
   );
 }
