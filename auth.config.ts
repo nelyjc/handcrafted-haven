8f30608b7fe60f4148b9caed2061514158ddbe01
RommelJuarez <cap03487@gmail.com>
2026-02-13 20:43:48 -0500

    Register protect route

--- auth.config.ts
+++ auth.config.ts
@@ -7,7 +7,7 @@ export const authConfig = {
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith("/profile");
+      const isOnDashboard = nextUrl.pathname.startsWith("/dashboard/profile");
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false;
